8f7a2491c023c206781d2afebe17ac1dc5fb37a2
diego.ramon <diego.ramon@pragma.com.co>
2021-10-21 16:17:59 -0500

    building accordion component

---
+++ src/components/accordion/index.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+import { Body, Container, Header, Inner, Item, Title } from "./styles/accordion";
+
+export default function Accordion({ children, ...restProps}) {
+  return (
+    <Container {...restProps}>
+      <Inner>{children}</Inner>
+    </Container>
+  )
+}
+
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
+  return <Item {...restProps}>{children}</Item>;
+} 
+
+Accordion.Title = function AccordionTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+}
+
+Accordion.Header = function AccordionHeader({ children, ...restProps }) {
+  return <Header {...restProps}>{children}</Header>;
+}
+
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+  return <Body {...restProps}>{children}</Body>;
+}
